b19365928d0e47bfe99733cca310e9a00dd93fcc
Tarrence van As <tarrence@vanas.family>
2022-01-04 14:22:31 -0500

    feat(provider): transaction id by hash, transaction hash by id

--- provider/transaction.go
+++ provider/transaction.go
@@ -2,10 +2,34 @@ package provider
 
 import (
 	"context"
+	"math/big"
 	"net/http"
+	"net/url"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/google/go-querystring/query"
 )
+
+type TransactionStatus struct {
+	TxStatus  string `json:"tx_status"`
+	BlockHash string `json:"block_hash"`
+}
+
+type Transaction struct {
+	Status      string `json:"status"`
+	Transaction struct {
+		EntryPointType     string   `json:"entry_point_type"`
+		EntryPointSelector string   `json:"entry_point_selector"`
+		ContractAddress    string   `json:"contract_address"`
+		TransactionHash    string   `json:"transaction_hash"`
+		Calldata           []string `json:"calldata"`
+		Signature          []string `json:"signature"`
+		Type               string   `json:"type"`
+	} `json:"transaction"`
+	BlockNumber      int    `json:"block_number"`
+	BlockHash        string `json:"block_hash"`
+	TransactionIndex int    `json:"transaction_index"`
+}
 
 type TransactionReceipt struct {
 	TransactionIndex   int `json:"transaction_index"`
@@ -28,19 +52,9 @@ type TransactionReceipt struct {
 	BlockNumber     int           `json:"block_number"`
 }
 
-type Transaction struct {
-	Signature          []interface{} `json:"signature"`
-	EntryPointSelector string        `json:"entry_point_selector"`
-	EntryPointType     string        `json:"entry_point_type"`
-	ContractAddress    string        `json:"contract_address"`
-	Calldata           []string      `json:"calldata"`
-	TransactionHash    string        `json:"transaction_hash"`
-	Type               string        `json:"type"`
-}
-
 type TransactionOptions struct {
-	TransactionIndex uint64 `url:"transactionId,omitempty"`
-	TransactionHash  string `url:"transactionHash,omitempty"`
+	TransactionId   uint64 `url:"transactionId,omitempty"`
+	TransactionHash string `url:"transactionHash,omitempty"`
 }
 
 // Gets the transaction information from a tx id.
@@ -59,4 +73,66 @@ func (p *Provider) Transaction(ctx context.Context, opts TransactionOptions) (*T
 
 	var resp Transaction
 	return &resp, p.do(req, &resp)
+}
+
+type TransactionStatusOptions struct {
+	TransactionId   uint64 `url:"transactionId,omitempty"`
+	TransactionHash string `url:"transactionHash,omitempty"`
+}
+
+// Gets the transaction status from a txn.
+//
+// [Reference](https://github.com/starkware-libs/cairo-lang/blob/fc97bdd8322a7df043c87c371634b26c15ed6cee/src/starkware/starknet/services/api/feeder_gateway/feeder_gateway_client.py#L87)
+func (p *Provider) TransactionStatus(ctx context.Context, opts TransactionStatusOptions) (*TransactionStatus, error) {
+	req, err := p.newRequest(ctx, http.MethodGet, "/get_transaction_status", nil)
+	if err != nil {
+		return nil, err
+	}
+	vs, err := query.Values(opts)
+	if err != nil {
+		return nil, err
+	}
+	appendQueryValues(req, vs)
+
+	var resp TransactionStatus
+	return &resp, p.do(req, &resp)
+}
+
+// Gets the transaction id from its hash.
+//
+// [Reference](https://github.com/starkware-libs/cairo-lang/blob/fc97bdd8322a7df043c87c371634b26c15ed6cee/src/starkware/starknet/services/api/feeder_gateway/feeder_gateway_client.py#L137)
+func (p *Provider) TransactionID(ctx context.Context, hash common.Hash) (*big.Int, error) {
+	req, err := p.newRequest(ctx, http.MethodGet, "/get_transaction_id_by_hash", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	appendQueryValues(req, url.Values{
+		"transactionHash": []string{hash.Hex()},
+	})
+
+	var resp big.Int
+	return &resp, p.do(req, &resp)
+}
+
+// Gets the transaction hash from its id.
+//
+// [Reference](https://github.com/starkware-libs/cairo-lang/blob/fc97bdd8322a7df043c87c371634b26c15ed6cee/src/starkware/starknet/services/api/feeder_gateway/feeder_gateway_client.py#L130)
+func (p *Provider) TransactionHash(ctx context.Context, id *big.Int) (*common.Hash, error) {
+	req, err := p.newRequest(ctx, http.MethodGet, "/get_transaction_hash_by_id", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	appendQueryValues(req, url.Values{
+		"transactionId": []string{id.String()},
+	})
+
+	var resp string
+	if err := p.do(req, &resp); err != nil {
+		return nil, err
+	}
+
+	hash := common.HexToHash(resp)
+	return &hash, nil
 }
--- provider/transaction_test.go
+++ provider/transaction_test.go
@@ -3,17 +3,21 @@ package provider
 import (
 	"context"
 	"encoding/json"
+	"math/big"
 	"testing"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 const transactionDump = `{"status": "ACCEPTED_ON_L2", "transaction": {"entry_point_type": "EXTERNAL", "entry_point_selector": "0x240060cdb34fcc260f41eac7474ee1d7c80b7e3607daff9ac67c7ea2ebb1c44", "contract_address": "0x26704e810b71eb2b99081e5093a01773f69b37163f8256c59dbd323bbab8b3d", "transaction_hash": "0x460c647079d51300fa578ae674d80f74febbb957143929c49599f86707a3850", "calldata": ["2997654770182471460954719312950884680023265536996318019468425755755830882165", "1329909728320632088402217562277154056711815095720684343816173432540100887380", "3", "1086644808292048134107134757058009361393820425772757521779863424964944956221", "1000000000000000000000", "0", "0"], "signature": ["2624462889305163041535477652824376920771700600796484323994636651237400740799", "1159601377385057436007767188985818160384069704534186283200528616638142189836"], "type": "INVOKE_FUNCTION"}, "block_number": 582, "block_hash": "0x182d83f0ed972e97fa529be0088e20b5a7cb32e3bba0d164d668147713e79f9", "transaction_index": 19}`
+const transactionStatusDump = `{"tx_status": "ACCEPTED_ON_L2", "block_hash": "0x182d83f0ed972e97fa529be0088e20b5a7cb32e3bba0d164d668147713e79f9"}`
 
 func TestProvider_Transaction(t *testing.T) {
-	var want *Transaction
+	var want Transaction
 	if err := json.Unmarshal([]byte(transactionDump), &want); err != nil {
-		t.Fatalf("unmarshalling block: %v", err)
+		t.Fatalf("unmarshalling: %v", err)
 	}
 
 	for _, tc := range []struct {
@@ -21,7 +25,7 @@ func TestProvider_Transaction(t *testing.T) {
 	}{{
 		opts: TransactionOptions{TransactionHash: "0x460c647079d51300fa578ae674d80f74febbb957143929c49599f86707a3850"},
 	}, {
-		opts: TransactionOptions{TransactionIndex: 19},
+		opts: TransactionOptions{TransactionId: 20343},
 	}} {
 		ctx := context.Background()
 		p := NewProvider("https://alpha-mainnet.starknet.io")
@@ -30,8 +34,66 @@ func TestProvider_Transaction(t *testing.T) {
 			t.Fatalf("getting transaction: %v", err)
 		}
 
-		if diff := cmp.Diff(want, got, nil); diff != "" {
+		if diff := cmp.Diff(&want, got, nil); diff != "" {
 			t.Errorf("Transaction diff mismatch (-want +got):\n%s", diff)
 		}
+	}
+}
+
+func TestProvider_TransactionStatus(t *testing.T) {
+	var want *TransactionStatus
+	if err := json.Unmarshal([]byte(transactionStatusDump), &want); err != nil {
+		t.Fatalf("unmarshalling: %v", err)
+	}
+
+	for _, tc := range []struct {
+		opts TransactionStatusOptions
+	}{{
+		opts: TransactionStatusOptions{TransactionHash: "0x460c647079d51300fa578ae674d80f74febbb957143929c49599f86707a3850"},
+	}, {
+		opts: TransactionStatusOptions{TransactionId: 20343},
+	}} {
+		ctx := context.Background()
+		p := NewProvider("https://alpha-mainnet.starknet.io")
+		got, err := p.TransactionStatus(ctx, tc.opts)
+		if err != nil {
+			t.Fatalf("getting transaction status: %v", err)
+		}
+
+		if diff := cmp.Diff(want, got, nil); diff != "" {
+			t.Errorf("TransactionStatus diff mismatch (-want +got):\n%s", diff)
+		}
+	}
+}
+
+func TestProvider_TransactionID(t *testing.T) {
+	ctx := context.Background()
+
+	want := big.NewInt(20343)
+	p := NewProvider("https://alpha-mainnet.starknet.io")
+	got, err := p.TransactionID(ctx, common.HexToHash("0x460c647079d51300fa578ae674d80f74febbb957143929c49599f86707a3850"))
+	if err != nil {
+		t.Fatalf("getting transaction ud: %v", err)
+	}
+
+	if diff := cmp.Diff(want, got, cmp.Options{
+		cmpopts.IgnoreUnexported(big.Int{}),
+	}); diff != "" {
+		t.Errorf("TransactionID diff mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestProvider_TransactionHash(t *testing.T) {
+	ctx := context.Background()
+
+	want := common.HexToHash("0x460c647079d51300fa578ae674d80f74febbb957143929c49599f86707a3850")
+	p := NewProvider("https://alpha-mainnet.starknet.io")
+	got, err := p.TransactionHash(ctx, big.NewInt(20343))
+	if err != nil {
+		t.Fatalf("getting transaction hash: %v", err)
+	}
+
+	if diff := cmp.Diff(&want, got); diff != "" {
+		t.Errorf("TransactionHash diff mismatch (-want +got):\n%s", diff)
 	}
 }
